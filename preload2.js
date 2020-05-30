4fb3b717a40112e9f58a7f32385e81acb72e86a9
BACKEND DEVELOPER <backend@Daniel-Salcedo-KONFIO-MacBook-Air.local>
2020-05-30 15:36:35 -0500

    Update timer new browser

--- preload2.js
+++ preload2.js
@@ -1,4 +1,21 @@
 const remote = require('electron').remote
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let times = [
+	7000,
+	10000,
+	15000,
+	20000,
+	30000,
+	35000,
+	40000,
+];
+let timer = getRandomInt(1, 7);
 setTimeout(function(){    	
 		remote.getCurrentWindow().close()
-}, 7000);
+}, timer - 1);
